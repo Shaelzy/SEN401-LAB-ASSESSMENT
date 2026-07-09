6a2d774307b8ed589905f207e24b03aba5640d44
Shaelzy <m.wayo7015@miva.edu.ng>
2026-07-09 22:16:52 +0100

    Update app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+# app.py
+
+from students import students
+from utils.helpers import highest_score, lowest_score, average_score
+
+
+def main():
+    print("===== STUDENT REPORT =====\n")
+
+    print("Student Records")
+    for student in students:
+        print(f"{student['name']} - {student['score']}")
+
+    highest = highest_score(students)
+    lowest = lowest_score(students)
+    average = average_score(students)
+
+    print("\nSummary")
+    print(f"Highest Score: {highest['name']} ({highest['score']})")
+    print(f"Lowest Score: {lowest['name']} ({lowest['score']})")
+    print(f"Average Score: {average:.2f}")
+
+
+if __name__ == "__main__":
+    main()
